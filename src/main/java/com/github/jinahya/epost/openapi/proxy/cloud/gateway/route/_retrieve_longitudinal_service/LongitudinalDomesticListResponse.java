e60287d143b851ae824f823e056d6a2cfd758bdb
Jin Kwon <onacit@gmail.com>
2024-07-29 18:53:32 +0900

    wip

---
+++ src/main/java/com/github/jinahya/epost/openapi/proxy/cloud/gateway/route/_retrieve_longitudinal_service/LongitudinalDomesticListResponse.java
@@ -0,0 +1,124 @@
+package com.github.jinahya.epost.openapi.proxy.cloud.gateway.route._retrieve_longitudinal_service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.jinahya.epost.openapi.proxy.cloud.gateway.route.__common.AbstractPairedResponseType;
+import com.github.jinahya.epost.openapi.proxy.cloud.gateway.route.__common.AbstractType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+import java.io.Serial;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+@Setter
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class LongitudinalDomesticListResponse
+        extends AbstractPairedResponseType<LongitudinalDomesticListResponse, LongitudinalDomesticListRequest> {
+
+    @Serial
+    private static final long serialVersionUID = 6933169509438831925L;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    static final String ROOT_NAME = "LongitudinalDomesticListResponse";
+
+    // -----------------------------------------------------------------------------------------------------------------
+    private static final String NAME_STATE_ENG_LIST = "longitudinalDomesticList";
+
+    @Setter
+    @Getter
+    @ToString(callSuper = true)
+    @EqualsAndHashCode(callSuper = true)
+    @NoArgsConstructor
+    public static class LongitudinalDomesticList
+            extends AbstractType<LongitudinalDomesticList> {
+
+        @Serial
+        private static final long serialVersionUID = 2060283902665793118L;
+
+        // ---------------------------------------------------------------------------------------------------- dlvyDate
+        public static final DateTimeFormatter DLVY_DATE_FORMATTER = DateTimeFormatter.ofPattern("uuuu.MM.dd");
+
+        // ---------------------------------------------------------------------------------------------------- dlvyTime
+        public static final DateTimeFormatter DLVY_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+        // ------------------------------------------------------------------------------------------------------ parent
+        public LongitudinalDomesticList parent(final LongitudinalDomesticListResponse parent) {
+            setParent(parent);
+            return this;
+        }
+
+        // ---------------------------------------------------------------------------------------------------- dlvyDate
+        @JsonIgnore
+        @XmlTransient
+        public LocalDate getDlvyDateAsLocalDate() {
+            return Optional.ofNullable(getDlvyDate())
+                    .map(v -> java.time.LocalDate.parse(v, DLVY_DATE_FORMATTER))
+                    .orElse(null);
+        }
+
+        // ---------------------------------------------------------------------------------------------------- dlvyTime
+        @JsonIgnore
+        @XmlTransient
+        public LocalTime getDlvyTimeAsLocalTime() {
+            return Optional.ofNullable(getDlvyTime())
+                    .map(v -> LocalTime.parse(v, DLVY_TIME_FORMATTER))
+                    .orElse(null);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------
+        @JsonIgnore
+        private transient LongitudinalDomesticListResponse parent;
+
+        // -------------------------------------------------------------------------------------------------------------
+        @XmlElement
+        private String dlvyDate;
+
+        @XmlElement
+        private String dlvyTime;
+
+        @XmlElement
+        private String nowLc;
+
+        @XmlElement
+        private String processSttus;
+
+        @Nullable
+        @XmlElement(nillable = true)
+        private String detailDc;
+    }
+
+    // ------------------------------------------------------------------------------------------ STATIC_FACTORY_METHODS
+
+    // ---------------------------------------------------------------------------------------------------- CONSTRUCTORS
+    public LongitudinalDomesticListResponse() {
+        super(LongitudinalDomesticListRequest.class);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    private String applcntNm;
+
+    private String addrseNm;
+
+    private String rgist;
+
+    private String pstmtrKnd;
+
+    @Nullable
+    @XmlElement(nillable = true)
+    private String trtmntSe;
+
+    private String dlvyDe;
+
+    private String dlvySttus;
+
+    private List<@Valid @NotNull LongitudinalDomesticList> longitudinalDomesticList;
+}
---
+++ src/test/java/com/github/jinahya/epost/openapi/proxy/cloud/gateway/route/_retrieve_longitudinal_service/LongitudinalDomesticListRequestTest.java
@@ -0,0 +1,20 @@
+package com.github.jinahya.epost.openapi.proxy.cloud.gateway.route._retrieve_longitudinal_service;
+
+import com.github.jinahya.epost.openapi.proxy.cloud.gateway.route.__common.AbstractPairedRequestTypeTest;
+import nl.jqno.equalsverifier.api.SingleTypeEqualsVerifierApi;
+
+class LongitudinalDomesticListRequestTest
+        extends AbstractPairedRequestTypeTest<LongitudinalDomesticListRequest, LongitudinalDomesticListResponse> {
+
+    LongitudinalDomesticListRequestTest() {
+        super(LongitudinalDomesticListRequest.class);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    protected SingleTypeEqualsVerifierApi<LongitudinalDomesticListRequest> __equals(
+            final SingleTypeEqualsVerifierApi<LongitudinalDomesticListRequest> verifierApi) {
+        return super.__equals(verifierApi);
+    }
+}
