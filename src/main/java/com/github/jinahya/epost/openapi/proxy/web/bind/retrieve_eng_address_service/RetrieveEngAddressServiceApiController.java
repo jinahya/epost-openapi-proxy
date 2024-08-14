679d82c5498a5854bcfe73f7bc9ffd34312f5b22
Jin Kwon <onacit@gmail.com>
2024-08-14 11:35:46 +0900

    wip

--- src/main/java/com/github/jinahya/epost/openapi/proxy/web/bind/retrieve_eng_address_service/RetrieveEngAddressServiceApiController.java
+++ src/main/java/com/github/jinahya/epost/openapi/proxy/web/bind/retrieve_eng_address_service/RetrieveEngAddressServiceApiController.java
@@ -1,23 +1,32 @@
 package com.github.jinahya.epost.openapi.proxy.web.bind.retrieve_eng_address_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.jinahya.epost.openapi.proxy.cloud.gateway.route.retrieve_eng_address_service.StateEngListRequest;
 import com.github.jinahya.epost.openapi.proxy.cloud.gateway.route.retrieve_eng_address_service.hateoas.*;
+import com.github.jinahya.epost.openapi.proxy.http.codec.json._Jackson2JsonEncoder;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.reactive.context.ReactiveWebServerInitializedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.ResolvableType;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Hidden
 @Tag(name = _Constants.TAG)
@@ -28,8 +37,13 @@ import reactor.core.publisher.Flux;
 @Slf4j
 class RetrieveEngAddressServiceApiController {
 
+    private static final ResolvableType RESOLVABLE_TYPE_FOR_STATE = ResolvableType.forType(State.class);
+
     @EventListener
     void onApplicationEvent(final ReactiveWebServerInitializedEvent event) {
+        objectMapper = this.objectMapperBuilder
+                .featuresToDisable(SerializationFeature.INDENT_OUTPUT)
+                .build();
         webClient = WebClient.builder()
                 .baseUrl("http://localhost:" + event.getWebServer().getPort())
                 .build();
@@ -43,13 +57,26 @@ class RetrieveEngAddressServiceApiController {
                     MediaType.APPLICATION_NDJSON_VALUE
             }
     )
-    Flux<State> readStates() {
-//        return new StateEngListRequest()
-//                .exchange(webClient)
-//                .flatMapMany(r -> Flux.fromIterable(r.getStateEngList()))
-//                .map(State::stateOf)
-//                ;
-        throw new UnsupportedOperationException("");
+    Mono<Void> readStates(final ServerHttpResponse response) {
+        response.beforeCommit(() -> Mono.fromRunnable(() -> {
+            response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_NDJSON_VALUE);
+        }));
+        return response.writeWith(
+                new StateEngListRequest()
+                        .exchange(webClient)
+                        .flatMapMany(r -> Flux.fromIterable(r.getStateEngList()))
+                        .map(State::stateOf)
+                        .map(State::addLinks)
+                        .map(s -> new _Jackson2JsonEncoder(objectMapper).encodeValue(
+                                s,
+                                response.bufferFactory(),
+                                RESOLVABLE_TYPE_FOR_STATE,
+                                null,
+                                null
+                        ))
+                        .map(db -> db.ensureWritable(1).write((byte) 0x0A))
+        );
+//        throw new UnsupportedOperationException("");
     }
 
     // ---------------------------------------------------------------------------------- /.../states/{stateName}/cities
@@ -121,5 +148,12 @@ class RetrieveEngAddressServiceApiController {
     }
 
     // -----------------------------------------------------------------------------------------------------------------
+    @Autowired
+    private Jackson2ObjectMapperBuilder objectMapperBuilder;
+
+    private ObjectMapper objectMapper;
+
+//    private HttpMessageEncoder<Object> jsonEncoder = new _Jackson2JsonEncoder(objectMapper);
+
     private WebClient webClient;
 }
